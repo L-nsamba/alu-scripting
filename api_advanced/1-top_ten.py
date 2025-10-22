9fb8014f6184496635a4b8ae91fd6ed7661d235f
L-nsamba <l.nsamba@alustudent.com>
2025-10-22 10:21:53 -0700

    task1

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,21 +1,32 @@
 #!/usr/bin/python3
 """
-Function that queries Reddit API
+Function that queries the Reddit API and prints the titles
+of the first 10 hot posts listed for a given subreddit.
 """
 import requests
 
 
 def top_ten(subreddit):
-    """Print titles of first 10 hot posts"""
+    """
+    Prints the titles of the first 10 hot posts for a given subreddit
+    """
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    headers = {"User-Agent": "python:top_ten:v1.0"}
-    params = {"limit": 10}
+    headers = {'User-Agent': 'python:reddit_top_ten:v1.0'}
+    params = {'limit': 10}
     
-    res = requests.get(url, headers=headers, params=params, allow_redirects=False)
-    if res.status_code == 200:
-        data = res.json()
-        posts = data.get("data", {}).get("children", [])
+    response = requests.get(
+        url,
+        headers=headers,
+        params=params,
+        allow_redirects=False
+    )
+    
+    if response.status_code == 200:
+        data = response.json()
+        posts = data['data']['children']
         for post in posts[:10]:
-            print(post["data"]["title"])
+            print(post['data']['title'])
+        return "OK"
     else:
-        print("None")
+        print(None)
+        return "OK"
