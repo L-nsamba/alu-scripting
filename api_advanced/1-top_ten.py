ff863814d201e8ae78bbe4157ed86df1c1acefef
L-nsamba <l.nsamba@alustudent.com>
2025-10-22 10:01:03 -0700

    task1

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """
-Function that queries the Reddit API and prints the titles
-of the first 10 hot posts listed for a given subreddit.
+Function that queries the Reddit API and prints the titles of the first 10 hot posts
 """
 import requests
 
@@ -10,25 +9,17 @@ def top_ten(subreddit):
     """
     Prints the titles of the first 10 hot posts for a given subreddit
     """
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
-    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36'}
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    headers = {'User-Agent': 'python:reddit_top_ten:v1.0'}
     params = {'limit': 10}
     
-    try:
-        response = requests.get(
-            url,
-            headers=headers,
-            params=params,
-            allow_redirects=False,
-            timeout=5
-        )
-        
-        if response.status_code == 200:
-            data = response.json()
-            posts = data['data']['children']
-            for post in posts[:10]:
-                print(post['data']['title'])
-        else:
-            print(None)
-    except:
+    response = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
+    
+    if response.status_code == 200:
+        data = response.json()
+        posts = data['data']['children']
+        for post in posts:
+            print(post['data']['title'])
+    else:
         print(None)
