28d1c140efca4f7f01083e9554a2b0b4d17bf4a3
L-nsamba <l.nsamba@alustudent.com>
2025-10-22 10:18:05 -0700

    task1

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,34 +1,21 @@
 #!/usr/bin/python3
 """
-Function that queries the Reddit API and prints the titles
-of the first 10 hot posts listed for a given subreddit.
+Function that queries Reddit API
 """
 import requests
 
 
 def top_ten(subreddit):
-    """
-    Prints the titles of the first 10 hot posts for a given subreddit
-    """
+    """Print titles of first 10 hot posts"""
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    headers = {'User-Agent': 'python:reddit_top_ten:v1.0'}
-    params = {'limit': 10}
+    headers = {"User-Agent": "python:top_ten:v1.0"}
+    params = {"limit": 10}
     
-    try:
-        response = requests.get(
-            url,
-            headers=headers,
-            params=params,
-            allow_redirects=False,
-            timeout=5
-        )
-        
-        if response.status_code == 200:
-            data = response.json()
-            posts = data['data']['children']
-            for post in posts[:10]:
-                print(post['data']['title'])
-            return True
-        return False
-    except:
-        return False
+    res = requests.get(url, headers=headers, params=params, allow_redirects=False)
+    if res.status_code == 200:
+        data = res.json()
+        posts = data.get("data", {}).get("children", [])
+        for post in posts[:10]:
+            print(post["data"]["title"])
+    else:
+        print("None")
