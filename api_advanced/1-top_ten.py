95b62f58029eec376219276f13125bf58a63c7c1
L-nsamba <l.nsamba@alustudent.com>
2025-10-22 06:17:53 -0700

    task1

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,22 +1,16 @@
 #!/usr/bin/python3
-"""Reddit API Query Module"""
+""" a function that queries the Reddit API and prints the titles of the first 10 hot posts listed for a given subreddit."""
 import requests
 
+
 def top_ten(subreddit):
-    """Queries Reddit API and prints titles of first 10 hot posts"""
-    url=f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
-    headers={'User-Agent':'python:reddit-api-client:v1.0'}
-    try:
-        response=requests.get(url,headers=headers,allow_redirects=False)
-        if response.status_code==200:
-            data=response.json()
-            posts=data['data']['children']
-            if posts:
-                for post in posts:
-                    print(post['data']['title'])
-            else:
-                print(None)
-        else:
-            print(None)
-    except Exception:
+    """ prints the titles of the first 10 hot posts listed in a subreddit """
+    url = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code != 200:
         print(None)
+        return
+    posts = response.json()['data']['children']
+    for post in posts:
+        print(post['data']['title'])
