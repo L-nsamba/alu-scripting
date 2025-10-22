30cf82b02aefa7aa5ec74809b653271388b1185e
L-nsamba <l.nsamba@alustudent.com>
2025-10-22 06:27:36 -0700

    task1

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,41 +1,19 @@
 #!/usr/bin/python3
-"""
-Module that queries the Reddit API and returns the titles
-of the first 10 hot posts for a given subreddit.
-"""
+""" Queries the Reddit API """
 import requests
 
 
 def top_ten(subreddit):
+    """ Prints the titles of the first 10 hot posts
+        listed for a given subreddit.
+        If an invalid subreddit is given, print None.
     """
-    Returns a list of the titles of the first 10 hot posts
-    for a given subreddit.
-    If not a valid subreddit, returns None.
-    """
-    if not subreddit or not isinstance(subreddit, str):
-        return None
-
-    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    headers = {"User-Agent": "My-User-Agent"}
-    params = {"limit": 10}
-
-    try:
-        response = requests.get(
-            url, headers=headers, params=params, allow_redirects=False
-        )
-
-        if response.status_code != 200:
-            return None
-
-        data = response.json().get("data", {})
-        posts = data.get("children", [])
-
-        if not posts:
-            return None
-
-        for post in posts:
-            print(post.get("data", {}).get("title"))
-
-        return True  # indicate success to checker
-    except Exception:
-        return None
+    user = {"User-Agent": "leonnsamba"}
+    req = requests.get("https://www.reddit.com/r/{}/hot.json?limit=10"
+                       .format(subreddit), headers=user)
+    if req.status_code != requests.codes.OK:
+        print('None')
+        return 0
+    else:
+        for child in req.json().get('data').get('children'):
+            print(child.get('data').get('title'))
