4fa3f8e94a9836054fff5646b3b127ea4d164793
L-nsamba <l.nsamba@alustudent.com>
2025-10-26 04:49:52 -0700

    task1

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,17 +1,34 @@
 #!/usr/bin/python3
-"""Prints the title of the first 10 hot posts listed for a given subreddit"""
+"""
+1-top_ten
+Module that queries the Reddit API and prints the titles of
+the first 10 hot posts for a given subreddit.
+"""
 
 import requests
 
 
 def top_ten(subreddit):
-    """Main function"""
-    URL = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
+    """Prints the titles of the first 10 hot posts for a given subreddit"""
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    headers = {"User-Agent": "Mozilla/5.0"}
+    params = {"limit": 10}
 
-    HEADERS = {"User-Agent": "PostmanRuntime/7.35.0"}
+    response = requests.get(url, headers=headers,
+                            params=params, allow_redirects=False)
+
+    # Check for valid subreddit
+    if response.status_code != 200:
+        print(None)
+        return
+
     try:
-        RESPONSE = requests.get(URL, headers=HEADERS, allow_redirects=False)
-        HOT_POSTS = RESPONSE.json().get("data").get("children")
-        [print(post.get('data').get('title')) for post in HOT_POSTS]
+        posts = response.json().get("data", {}).get("children", [])
+        if not posts:
+            print(None)
+            return
+
+        for post in posts:
+            print(post.get("data", {}).get("title"))
     except Exception:
         print(None)
